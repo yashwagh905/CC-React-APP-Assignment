42265d34f20d663ff356c16642dc430e3ec67b1c
yashwagh905 <123530501+yashwagh905@users.noreply.github.com>
2023-11-26 19:28:54 +0530

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first React Application
+          CC Assignment 5 Accomplished
         </p>
         <a
           className="App-link"
